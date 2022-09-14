8353e7e1b1fe5bccdc797eb5453094402a1b4290
Wender Barbosa <wenblack012@gmail.com>
2022-09-13 21:39:54 -0300

    Create base to web aplication

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+//Example of TypeScript Interface
+interface ButtonProps{
+  title : string
+}
+
+function Button (props :ButtonProps){
+  return(
+    <button>
+      {props.title}
+    </button>
+  )
+}
+
+
+function App() {
+  return (
+    <div>
+      <Button title="Button 1"/>
+      <Button title="Button 2"/>
+      <Button title="Button 3"/>
+    </div>
+  )
+}
+
+export default App
