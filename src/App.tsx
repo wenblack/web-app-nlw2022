09405304bc3a6c88dc4c693388cd563004887b8a
Wender Barbosa <wenblack012@gmail.com>
2022-09-13 22:10:34 -0300

    Download assets to folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,7 @@
-//Example of TypeScript Interface
-interface ButtonProps{
-  title : string
-}
-
-function Button (props :ButtonProps){
-  return(
-    <button>
-      {props.title}
-    </button>
-  )
-}
-
-
 function App() {
   return (
     <div>
-      <Button title="Button 1"/>
-      <Button title="Button 2"/>
-      <Button title="Button 3"/>
+      
     </div>
   )
 }
